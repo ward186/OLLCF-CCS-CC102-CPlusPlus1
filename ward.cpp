726a6279e0eee3a688a80f78595a2053e32223f8
Howard ALmayda <howardalmayda06@gmail.com>
2024-11-24 17:46:51 +0800

    optimized.cpp

--- ward.cpp
+++ ward.cpp
@@ -1,45 +1,36 @@
 #include <iostream>
-
 using namespace std;
 
-int main ()
-{	
-	int age;
-	double money;
-	bool parent;
-	string toPrintShow, toPrintMoney]
-	cout << "enter age: ";
-	cin >> age;
-	if(age<13) { 
-		cout << "With Parent? ";
-		cin >> parent;
-				toPrintShow = "PG & G show.";
-			else
-				toPrintShow = "G show.";
-	}
-	else if (age<) {
-		cout << with parents? ";
-		cin >> parent;
-		if (parent)
-			toPrintShow = "R, PG & Show. ";
-		else 
-			toPrintShow = "PG & G Show. ";
-	}
-	else 
-		toPrintShow = "R, PG & G Show. ";
-	cout << "enter money: ";
-	cin >> money;
-	if(money<7.5)
-		toPrintMoney = "not enough money.";
-	else if (money<10.5)
-		toPrintMoney = "Can go to matinee show. "; 
-	else
-		toPrintMoney = "can go to evening & matinee show.";
-	cout << toPrintShow << endl;
-	cout << toPrintMoney << endl;
-	return 0;
+int main() {
+    int age;
+    double money;
+    bool parent;
+    string toPrintShow, toPrintMoney;
+
+    cout << "Enter age: ";
+    cin >> age;
+
+    if (age < 13) {
+        cout << "With Parent? (1 for yes, 0 for no): ";
+        cin >> parent;
+        toPrintShow = parent ? "PG & G show." : "G show.";
+    } else if (age < 18) {
+        cout << "With Parent? (1 for yes, 0 for no): ";
+        cin >> parent;
+        toPrintShow = parent ? "R, PG & G show." : "PG & G show.";
+    } else {
+        toPrintShow = "R, PG & G show.";
+    }
+
+    cout << "Enter money: ";
+    cin >> money;
+
+    toPrintMoney = (money < 7.5) ? "Not enough money." :
+                   (money < 10.5) ? "Can go to matinee show." :
+                   "Can go to evening & matinee show.";
+
+    cout << toPrintShow << endl;
+    cout << toPrintMoney << endl;
+
+    return 0;
 }
-		
-	
-	
-	
