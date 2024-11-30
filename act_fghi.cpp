6f3befb8b42dfa1771b538d9aca8f9e25ebd13f6
Howard ALmayda <howardalmayda06@gmail.com>
2024-11-30 20:14:21 +0800

    Update act 1.cpp

--- act_fghi.cpp
+++ act_fghi.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    int i = 1;\
+    char symbol1, symbol2, symbol3;
 
-    while (i <= 10) {
-        cout << "Number: " << i << endl;
-        i++;
-    }
+    cout << "Enter 1st symbol to use (*, ^, #, $, @): ";
+    cin >> symbol1;
+    cout << "Enter 2nd symbol to use (*, ^, $, @): ";
+    cin >> symbol2;
+    cout << "Enter 3rd symbol to use (*, $, @): ";
+    cin >> symbol3;
+
+    cout << "\nYou picked: " << symbol1 << " " << symbol2 << " " << symbol3 << endl;
 
     return 0;
 }
