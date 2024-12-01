a98c76660cff34154d5f81ac0d7859c4a40b8383
Howard ALmayda <howardalmayda06@gmail.com>
2024-12-01 21:37:39 +0800

    Update atoi.cpp

--- atoi.cpp
+++ atoi.cpp
@@ -138,16 +138,18 @@ int main() {
 using namespace std;
 
 int main() {
-    cout << "i:" << endl;
+    int rows = 5;
 
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= 10; j++) {
+            cout << " ";
+        }
         for (int j = 1; j < i; j++) {
             cout << "^";
         }
         cout << "*" << endl;
     }
-
-    for (int i = 1; i <= 2; i++) {
+      for (int i = 1; i <= 2; i++) {
         for (int j = 1; j <= 10; j++) {
             cout << "#";
         }
@@ -157,15 +159,16 @@ int main() {
         cout << "*" << endl;
     }
 
-    for (int i = 4; i >= 1; i--) {
+    for (int i = rows - 0; i >= 1; i--) {
+        for (int j = 1; j <= 10; j++) {
+            cout << " ";
+        }
+
         for (int j = 1; j < i; j++) {
             cout << "^";
         }
+
         cout << "*" << endl;
     }
-
     return 0;
 }
-
-
-
